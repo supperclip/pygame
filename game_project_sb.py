0d0e7af2c7e2563965b35d5cdc2f840e0083e3e5
supperclip <supperclip@gmail.com>
2024-10-14 15:12:21 +0200

    Add files via upload

--- game_project_sb.py
+++ game_project_sb.py
@@ -23,14 +23,22 @@ chainsaw_2 = pygame.image.load("x/chain_enemy2.png").convert_alpha()
 chainsaw_2 = pygame.transform.rotate(chainsaw_2 , 270)
 chainsaw_2 = pygame.transform.scale(chainsaw_2 , (60,60))
 
+turret_1 = pygame.image.load("x/turret_frame_1.png").convert_alpha()
+turret_1 = pygame.transform.scale(turret_1 , (100,100))
+
+rocket = pygame.image.load("x/rocket_1.png").convert_alpha()
+rocket = pygame.transform.scale(rocket, (30,30))
+turret_1 = pygame.transform.rotate(turret_1 , 270)
+
+
 chainsaw_animation_list = [chainsaw_1 , chainsaw_2]
 
-sentry_locX = [600,400]
-sentry_locY = [400,100]
-
 current_frame = 0
+frame_offset = 0
 enemy_Xlist = []
 enemy_Ylist = []
+
+turret_location = [801, 501]
 
 enemy_speed = 1 #pixels per frame?
 
@@ -40,6 +48,15 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit() 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                turret_location = [127.5,425]
+            if event.key == pygame.K_2:
+                turret_location = [327.5,425]
+            if event.key == pygame.K_3:
+                turret_location = [477.5,425]
+            if event.key == pygame.K_4:
+                turret_location = [677.5,425]
           
 
     # Mouse logic
@@ -48,11 +65,30 @@ while True:
     #background
     screen.blit(background_surface, (0, 0))
 
-    #blit static images
-    screen.blit(sentry_picture, (650,100))
+    #turret logic
+    screen.blit(sentry_picture, (100,400))
+    screen.blit(sentry_picture, (300,400))
+    screen.blit(sentry_picture, (450,400))
     screen.blit(sentry_picture, (650,400))
-    screen.blit(sentry_picture, (150,100))
-    screen.blit(sentry_picture, (150,400))
+
+    #screen.blit(turret_1 , (turret_location))
+
+    #rotate turret to mouse
+    TrotX = mouse_x - turret_location[0]
+    TrotY = mouse_y - turret_location[1]
+    Tdist = math.hypot(TrotX, TrotY)
+    Tdist += 0.000001 #fixes the crash, hopefuly?
+    TdirX = TrotX / Tdist
+    TdirY = TrotY / Tdist
+    Tangle_radians = math.atan2(TrotY, TrotX) 
+    Tangle_degrees = math.degrees(Tangle_radians)
+    rotated_turret = pygame.transform.rotate(turret_1, -Tangle_degrees)
+    turret_rect = rotated_turret.get_rect(center=(turret_location[0], turret_location[1]))
+
+    screen.blit(rotated_turret , (turret_rect.topleft))
+
+
+
     
     #enemy logic
     current_frame += 1
@@ -69,22 +105,23 @@ while True:
         # Calculate the rotation angle
         rotX = mouse_x - enemy_coords[0]
         rotY = mouse_y - enemy_coords[1]
-        #help
+        #this works, sadly
         dist = math.hypot(rotX, rotY)
-
+        dist += 0.000001 #fixes the crash, hopefuly?
         dirX = rotX / dist
         dirY = rotY / dist
-
         enemy_Xlist[x] += dirX * enemy_speed
         enemy_Ylist[x] += dirY * enemy_speed
-
         angle_radians = math.atan2(rotY, rotX) 
         angle_degrees = math.degrees(angle_radians)
+
+        if current_frame % 10 == 0:
+            frame_offset = random.randint(0,1)
 
         #distance mouse to enemy with math vodoo shit
         
         #idk how this works but it works 
-        rotated_enemy = pygame.transform.rotate(chainsaw_animation_list[random.randint(0,1)], -angle_degrees)
+        rotated_enemy = pygame.transform.rotate(chainsaw_animation_list[frame_offset], -angle_degrees)
         
         #get new surface with more vodoo
         rotated_rect = rotated_enemy.get_rect(center=(enemy_coords[0], enemy_coords[1]))
@@ -95,4 +132,4 @@ while True:
     pygame.display.update()
     clock.tick(60)
 
-    #known issues: game can crash because distance can be 0 (rare) and crash the game
+    #known issues: game can crash because distance can be 0 and it is used to devide another number (fixed?)
