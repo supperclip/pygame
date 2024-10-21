d3c4cb8b7fedea0702ae8c3fcccc8f49f729c57a
supperclip <supperclip@gmail.com>
2024-10-21 16:07:41 +0200

    Update player_class.py

--- player_class.py
+++ player_class.py
@@ -1,27 +1,36 @@
-class player:
-    def __init__(self, MoveRight,MoveLeft,MoveUp,MoveDown):
-        self.MoveRight = MoveRight
-        self.Left = MoveLeft
-        self.MoveUp = MoveUp
-        self.MoveDown = MoveDown
+from enum import Enum
 
-    def MoveX_function(self,MoveRight):
-        if (MoveRight == True):
-            return 2.5
-        else:
-            return 0
-    def MoveXMinus_function(self,MoveLeft):
-        if (MoveLeft == True):
-            return -2.5
-        else:
-            return 0
-    def MoveY_function(self,MoveUp):
-        if (MoveUp == True):
-            return 2.5
-        else:
-            return 0
-    def MoveYMinus_function(self,MoveDown):
-        if (MoveDown == True):
-            return -2.5
-        else:
-            return 0
+class Directions(Enum):
+    InValid = 0
+    Up = 1
+    Down = 2
+    Right = 3
+    Left = 4
+    UpAndRight = 5
+    UpAndLeft = 6
+    DownAndRight = 7
+    DownAndLeft = 8
+
+class player:
+    def __init__(self,PlayerDirection):
+        self.PlayerDirection = PlayerDirection
+    
+    def MovePlayer(self, PlayerDirection):
+        if PlayerDirection == Directions.InValid:
+            return [0,0]
+        if PlayerDirection == Directions.UpAndLeft:
+            return [-1,-1]
+        if PlayerDirection == Directions.UpAndRight:
+            return [1,-1]
+        if PlayerDirection == Directions.DownAndLeft:
+            return [-1,1]
+        if PlayerDirection == Directions.DownAndRight:
+            return [1,1]
+        elif PlayerDirection == Directions.Right:
+            return [1,0]
+        elif PlayerDirection == Directions.Left:
+            return [-1,0]
+        elif PlayerDirection == Directions.Up:
+            return [0,-1]
+        elif PlayerDirection == Directions.Down:
+            return [0,1]
